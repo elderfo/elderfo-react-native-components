e3573b19558107c1ce8181250a9229bd3ba73985
Freddy <cgetsfred@gmail.com>
2016-12-29 18:50:54 -0600

    fix(style-merging): Switched to lodash.merge
    Converted over to use the lodash.merge package.

--- src/Util/computeStyle.js
+++ src/Util/computeStyle.js
@@ -1,9 +1,8 @@
-import cloneDeep from 'lodash/cloneDeep';
-import merge from 'lodash/merge';
+import merge from 'lodash.merge';
 
 export default function computeStyle(defaultStyle, newStyle) {
     if (typeof (newStyle) !== 'object') {
-        return cloneDeep(defaultStyle);
+        return merge({}, defaultStyle);
     }
     return merge({}, defaultStyle, newStyle);
 }
