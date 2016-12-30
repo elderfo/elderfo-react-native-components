541bc2ffa0271b056f1c70d0be4d734412df8253
Freddy <cgetsfred@gmail.com>
2016-12-29 19:54:17 -0600

    test(container): Added stories for Container
    Closes #2

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -3,3 +3,4 @@ import '../../src/Components/Card.stories';
 import '../../src/Components/Header.stories';
 import '../../src/Components/Typeography.stories';
 import '../../src/Components/CenterView.stories';
+import '../../src/Components/Container.stories';
