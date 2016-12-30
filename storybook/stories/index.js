824865ac8616f1f995f9ae5058018308275af638
Freddy <cgetsfred@gmail.com>
2016-12-29 20:34:08 -0600

    test(erroralert): Added story for ErrorAlert
    fix #3

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,6 +1,7 @@
 import '../../src/Components/BusyModal.stories';
 import '../../src/Components/Card.stories';
-import '../../src/Components/Header.stories';
-import '../../src/Components/Typeography.stories';
 import '../../src/Components/CenterView.stories';
 import '../../src/Components/Container.stories';
+import '../../src/Components/ErrorAlert.stories';
+import '../../src/Components/Header.stories';
+import '../../src/Components/Typeography.stories';
